f1bb0833784663351f1270a66ba1eeebb7514cc5
Karan7389 <karan@cognhire.in>
2026-06-08 10:44:01 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000/api"
-    : "https://clinic-backend-production-82da.up.railway.app/api";
+    : "https://clinic-backend-production-82da.up.railway.app";
 
 const API = axios.create({
   baseURL: BASE_URL,
