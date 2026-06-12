1fd798244de4790e58129c34fc4352da8ead11c6
Karan7389 <karan@cognhire.in>
2026-06-12 11:38:34 +0530

    Update API base URL to include '/api' path

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000/"
-    : "https://clinic-backend-production-9e35.up.railway.app";
+    : "https://clinic-backend-production-9e35.up.railway.app/api";
 
 const API = axios.create({
   baseURL: BASE_URL,
